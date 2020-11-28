3626526f48bf8f223479682a45adc8a965498e1f
ezequiel veltri <ezequiel.veltri@istea.com.ar>
2020-11-28 13:53:32 -0300

    ok

---
+++ EnviarCapturaBackend/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnviarCapturaBackend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }        
+
+        [HttpPost]
+        public async Task<ActionResult> RecibirFoto(String imageData)
+        {
+            string fileNameWitPath = Server.MapPath("~/fotos/mifoto.png");
+            try
+            {
+                using (FileStream fs = new FileStream(fileNameWitPath, FileMode.Create))
+                {
+                    using (BinaryWriter bw = new BinaryWriter(fs))
+                    {
+                        byte[] data = Convert.FromBase64String(imageData);                        
+                        bw.Write(data);
+                        bw.Close();
+                    }
+                }
+                return Json("true");
+            }
+            catch (Exception)
+            {
+                return Json("false");
+                throw;
+            }          
+
+        }
+
+    }
+}
---
+++ EnviarCapturaBackend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnviarCapturaBackend.MvcApplication" Language="C#" %>
